62d81b892f92b1c2b80dce223213d02ecf298cca
nurdle <50721698+nurd3@users.noreply.github.com>
2023-03-15 09:45:06 -0400

    added community features

---
+++ community/level/script.js
@@ -0,0 +1,47 @@
+/* yes this is under GPL v3.0
+ * 
+ * yes this code sucks
+ * unfortunately i don't care
+ *
+ * code by Dante Davis
+ *     started: 2022/03/09 (YYYY/MM/DD)
+ * last edited: 2022/03/10
+ */
+const ws = new WebSocket('wss://server.4j89.repl.co');
+
+ws.addEventListener('message', (event) => {
+    var response = event.data;
+	if (response == "level not found" || response == "server error") {
+		window.alert(response);
+		return;
+	}
+	var data = 
+			JSON.parse(response);
+	level.map = data.map;
+	level.magic = data.magic;
+	level.name = fromB64(data.name);
+	level.author = fromB64(data.author);
+	level.description = fromB64(data.description);
+	level.startPos = data.startPos;
+	
+	loadInfo({
+		"name": level.name,
+		"author": level.author,
+		"description": level.description
+	});
+	loadPlayer();
+});
+ws.addEventListener('open', (event) => {
+	if (id != -1)
+		ws.send("level:" + id);
+});
+var id = -1;
+var url = document.location.href.split("?id=");
+if (url.length > 1) {
+	id = url[1];
+}
+var level = {
+	"name": "...loading...",
+	"author": "...loading...",
+	"description": "...loading..."
+};
--- script.js
+++ script.js
@@ -13,15 +13,15 @@ document.body.innerHTML = `
 	<p>welcome all to Juphump, avoid the black and touch the green.</p>
 	<a href="story"><h3>Campaign</h3></a>
 	<a href="editor"><h3>Editor</h3></a>
-	<h2>Community:</h2>
-	<div class="border">
-		<a href="levels"><p>User Levels (Not Ready)</p></a>
-		<a href="stats"><p>Leaderboard (Not Ready)</p></a>
-		<a href="https://discord.gg/UAwVuBQx"><p>Discord Server</p></a>
-	</div>
-	<h2>Credits:</h2>
-	<div class="border">
-		<a href="https://github.com/dtinth/comic-mono-font"><p>Comic Mono Font</p></a>
-	</div>
+</div>
+<a href="/community"><h2>Community</h2></a>
+<div class="border">
+	<a href="/community/levels"><p>User Levels (Under Development)</p></a>
+	<a href="/community/stats"><p>Leaderboard (Haven't Started Yet)</p></a>
+	<a href="https://discord.gg/UAwVuBQx"><p>Discord Server</p></a>
+</div>
+<h2>Credits</h2>
+<div class="border">
+	<a href="https://github.com/dtinth/comic-mono-font"><p>Comic Mono Font</p></a>
 </div>
 `;
